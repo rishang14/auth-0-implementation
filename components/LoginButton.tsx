2cd58a889d59214c90de1c67fb2811a79792e82a
Rishang <rishangkumar9693@gmail.com>
2025-11-14 15:40:48 +0530

    refactor: replace Link component with anchor tag in LoginButton for improved simplicity

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,13 +1,13 @@
 
-import Link from "next/link";
+
 
 export default function LoginButton() {
   return (
-    <Link
+    <a
       href="/auth/login"
       className="text-white bg-linear-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-linear-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
     >
       Log In
-    </Link>
+    </a>
   );
 }
